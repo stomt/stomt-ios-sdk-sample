b58e925fed8faafb48acb1cd0014b3ecf44446e0
Max Klenk <max@klenk.biz>
2015-10-13 15:32:31 +0200

    add open CreateStomtModal and Sign in/out buttons

--- stomt-ios-sdk-sample/ViewController.m
+++ stomt-ios-sdk-sample/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <Stomt-iOS-SDK/Stomt.h>
 
 @interface ViewController ()
 
@@ -16,12 +17,49 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    // Setup Stomt with your AppID
+	// -> get yours at: https://www.stomt.com/dev/my-apps
+    //
+    [Stomt setAppID:@"9oKmFftlRL946JtDDUTmpmW4H"];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)openStomtCreateModal:(UIButton *)sender {
+
+	// Open a creation modal for your applications page
+	// -> the targetID is your pages identifier you can copy it from the pages url
+	//    https://www.stomt.com/stomt-ios-sdk -> stomt-ios-sdk
+	//
+	[Stomt presentStomtCreationPanelWithTargetID:@"stomt-ios-sdk"
+									 defaultText:@" "
+									  likeOrWish:kSTObjectWish
+								 completionBlock:^(NSError *error, STObject *stomt) {
+		if(stomt) {
+			NSLog(@"Stomt created");
+		}
+	}];
+}
+
+- (IBAction)openLoginModal:(id)sender {
+	
+	// Users of stomt can login using oAuth to create stomts in their own name
+	//
+	[Stomt promptAuthenticationIfNecessaryWithCompletionBlock:^(BOOL succeeded, NSError *error, STUser *user) {
+		if(succeeded) {
+			NSLog(@"Successful logged in");
+		}
+	}];
+}
+
+- (IBAction)logout:(id)sender {
+	
+	// There is a logout function aswell
+	//
+	[Stomt logout];
 }
 
 @end
